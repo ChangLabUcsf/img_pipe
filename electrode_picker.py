dd25b2617e88deacb76284140f931d17941ec470
Liberty Hamilton <libertyhamilton@gmail.com>
2017-02-13 16:46:35 -0800

    Support for key press

--- electrode_picker.py
+++ electrode_picker.py
@@ -1,39 +1,41 @@
-#!/usr/env/python
+#!/usr/env/pytho
+import matplotlib
+matplotlib.use('Qt4Agg') 
 from matplotlib import pyplot as plt
 from matplotlib import cm
 import numpy as np
 import nibabel as nib
 import scipy.ndimage
 
-#img = nib.load('/Applications/freesurfer/subjects/EC121_test/acpc/T1.nii')
-#ct = nib.load('/Applications/freesurfer/subjects/EC121_test/CT/rCT.nii')
-# img = nib.load('/Applications/freesurfer/subjects/EC143/mri/orig.nii')
-# ct = nib.load('/Applications/freesurfer/subjects/EC143/CT/rCT.nii')
+img = nib.load('/Applications/freesurfer/subjects/EC121_test/acpc/T1.nii')
+ct = nib.load('/Applications/freesurfer/subjects/EC121_test/CT/rCT.nii')
+img = nib.load('/Applications/freesurfer/subjects/EC143/mri/orig.nii')
+ct = nib.load('/Applications/freesurfer/subjects/EC143/CT/rCT.nii')
 
-# affine = img.affine
-# codes = nib.orientations.axcodes2ornt(nib.orientations.aff2axcodes(affine))
-# img_data = nib.orientations.apply_orientation(img.get_data(), codes)
-# voxel_sizes = nib.affines.voxel_sizes(affine)
-# nx,ny,nz=np.array(img_data.shape, dtype='float')
+affine = img.affine
+codes = nib.orientations.axcodes2ornt(nib.orientations.aff2axcodes(affine))
+img_data = nib.orientations.apply_orientation(img.get_data(), codes)
+voxel_sizes = nib.affines.voxel_sizes(affine)
+nx,ny,nz=np.array(img_data.shape, dtype='float')
 
-# inv_affine = np.linalg.inv(affine)
-# img_clim = np.percentile(img_data, (1., 99.))
+inv_affine = np.linalg.inv(affine)
+img_clim = np.percentile(img_data, (1., 99.))
 
-# #ct_data = ct.get_data()
-# ct_codes =nib.orientations.axcodes2ornt(nib.orientations.aff2axcodes(ct.affine))
-# ct_data = nib.orientations.apply_orientation(ct.get_data(), ct_codes)
+#ct_data = ct.get_data()
+ct_codes =nib.orientations.axcodes2ornt(nib.orientations.aff2axcodes(ct.affine))
+ct_data = nib.orientations.apply_orientation(ct.get_data(), ct_codes)
 
-# cx,cy,cz=np.array(ct.shape, dtype='float')
-# current_slice = np.array([nx/2, ny/2, nz/2], dtype=np.int)
+cx,cy,cz=np.array(ct.shape, dtype='float')
+current_slice = np.array([nx/2, ny/2, nz/2], dtype=np.int)
 
-# # Resample both images to the highest resolution
-# voxsz = (256, 256, 256)
-# if ct.shape != voxsz:
-# 	print("Resizing voxels in CT")
-# 	ct_data = scipy.ndimage.zoom(ct_data, [voxsz[0]/cx, voxsz[1]/cy, voxsz[2]/cz])
-# if img.shape != voxsz:
-# 	print("Resizing voxels in MRI")
-# 	img_data = scipy.ndimage.zoom(img_data, [voxsz[0]/nx, voxsz[1]/ny, voxsz[2]/nz])
+# Resample both images to the highest resolution
+voxsz = (256, 256, 256)
+if ct.shape != voxsz:
+	print("Resizing voxels in CT")
+	ct_data = scipy.ndimage.zoom(ct_data, [voxsz[0]/cx, voxsz[1]/cy, voxsz[2]/cz])
+if img.shape != voxsz:
+	print("Resizing voxels in MRI")
+	img_data = scipy.ndimage.zoom(img_data, [voxsz[0]/nx, voxsz[1]/ny, voxsz[2]/nz])
 
 nx,ny,nz = [256, 256, 256]
 cx,cy,cz = [256, 256, 256]
@@ -52,12 +54,11 @@ ct_im.append(plt.imshow(ct_data[nx/2,:,:].T, cmap=cm.hot, aspect='auto',alpha=0.
 cursor.append(plt.plot([ny/2, ny/2], [ax1.get_ylim()[0]+1, ax1.get_ylim()[1]-1], color=[0, 1, 0] ))
 cursor2.append(plt.plot([ax1.get_xlim()[0]+1, ax1.get_xlim()[1]-1], [nz/2, nz/2], color=[0, 1, 0] ))
 plt.gca().invert_yaxis()
-ax1.axis([0,ny,0,nz])
 ax1.set_xticks([])
 ax1.set_yticks([])
 ax1.set_xlabel('Inferior')
 ax1.set_ylabel('Posterior')
-ax11 = ax1.twinx()
+ax1.axis([0,ny,0,nz])
 
 ax2 = fig.add_subplot(222)
 ax2.set_axis_bgcolor('k')
@@ -106,27 +107,22 @@ def on_key(event):
 	bb2=ax2.get_position()
 	bb3=ax3.get_position()
 	bb4=ax4.get_position()
+
 	# Transform coordinates to figure coordinates
-	fxy = fig.transFigure.inverted().transform((event.x, event.y))
-	if current_slice[0]+event.step >= 0 and current_slice[0]+event.step <= nx:
-		# If you clicked the first subplot
+	print event.x, event.y
+	if event.key == 'pageup':
 		if bb1.contains(fxy[0],fxy[1]):
-			current_slice[0]=current_slice[0]+event.step
-			im[0].set_data(img_data[current_slice[0],:,:].T)
-			
-		# If you clicked the second subplot
+			current_slice[0] = current_slice[0] + 1
 		elif bb2.contains(fxy[0],fxy[1]):
-			current_slice[1]=current_slice[1]+event.step
-			im[1].set_data(img_data[:,current_slice[1],:].T)
-			
-		# If you clicked the third subplot
+			current_slice[1] = current_slice[1] + 1
 		elif bb3.contains(fxy[0],fxy[1]):
-			current_slice[2]=current_slice[2]+event.step
-			im[2].set_data(img_data[:,:,current_slice[2]].T)
+			current_slice[2] = current_slice[2] + 1
+
+	if event.key == 'pagedown':
+		break;
+
 
 	plt.gcf().canvas.draw()
-
-cid = fig.canvas.mpl_connect('key_press_event', on_key)
 
 def on_scroll(event):
 	# Zoom on scroll
@@ -135,6 +131,7 @@ def on_scroll(event):
 	bb2=ax2.get_position()
 	bb3=ax3.get_position()
 	bb4=ax4.get_position()
+
 	# Transform coordinates to figure coordinates
 	fxy = fig.transFigure.inverted().transform((event.x, event.y))
 	
@@ -152,10 +149,9 @@ def on_scroll(event):
 		ylims = ax2.get_ylim()
 		xstep = event.step*stepsz
 		ystep = event.step*stepsz*np.float(nz)/nx
-		print xstep, ystep
-		print xlims, ylims
 		ax2.set_xlim(xlims[0]+xstep, xlims[1] - xstep)
 		ax2.set_ylim(ylims[0]+ystep, ylims[1] - ystep)
+
 	# If you clicked the third subplot
 	if bb3.contains(fxy[0],fxy[1]):
 		xlims = ax3.get_xlim()
@@ -165,9 +161,16 @@ def on_scroll(event):
 		ax3.set_xlim(xlims[0]+xstep, xlims[1] - xstep)
 		ax3.set_ylim(ylims[0]+ystep, ylims[1] - ystep)
 
-	plt.gcf().canvas.draw()
+	# If you clicked the fourth subplot
+	if bb4.contains(fxy[0],fxy[1]):
+		xlims = ax4.get_xlim()
+		ylims = ax4.get_ylim()
+		xstep = event.step*stepsz*np.float(nz)/ny
+		ystep = event.step*stepsz
+		ax4.set_xlim(xlims[0]+xstep, xlims[1] - xstep)
+		ax4.set_ylim(ylims[0]+ystep, ylims[1] - ystep)
 
-cid2 = fig.canvas.mpl_connect('scroll_event',on_scroll)
+	plt.gcf().canvas.draw()
 
 def on_click(event):
 	#print('You scrolled %d steps at x: %d, y: %d', event.step, event.x, event.y)
@@ -176,16 +179,21 @@ def on_click(event):
 	bb2=ax2.get_position()
 	bb3=ax3.get_position()
 	bb4=ax4.get_position()
+
 	print event.xdata, event.ydata
 	# Transform coordinates to figure coordinates
 	fxy = fig.transFigure.inverted().transform((event.x, event.y))
 	
+	x = np.int(np.round(event.xdata))
+	y = np.int(np.round(event.ydata))
 	# If you clicked the first subplot
 	if bb1.contains(fxy[0],fxy[1]):
-		im[1].set_data(img_data[:,event.xdata,:].T)
-		im[2].set_data(img_data[:,:,event.ydata].T)
-		ct_im[1].set_data(ct_data[:,event.xdata,:].T)
-		ct_im[2].set_data(ct_data[:,:,event.ydata].T)
+		current_slice[1] = event.xdata
+		current_slice[2] = event.ydata
+		im[1].set_data(img_data[:,x,:].T)
+		im[2].set_data(img_data[:,:,y].T)
+		ct_im[1].set_data(ct_data[:,x,:].T)
+		ct_im[2].set_data(ct_data[:,:,y].T)
 		cursor[0][0].set_xdata([event.xdata, event.xdata])
 		cursor2[0][0].set_ydata([event.ydata, event.ydata])
 		cursor2[1][0].set_ydata([event.ydata, event.ydata])
@@ -195,10 +203,12 @@ def on_click(event):
 		
 	# If you clicked the second subplot
 	elif bb2.contains(fxy[0],fxy[1]):
-		im[0].set_data(img_data[event.xdata,:,:].T)
-		im[2].set_data(img_data[:,:,event.ydata].T)
-		ct_im[0].set_data(ct_data[event.xdata,:,:].T)
-		ct_im[2].set_data(ct_data[:,:,event.ydata].T)
+		current_slice[0] = event.xdata
+		current_slice[2] = event.ydata
+		im[0].set_data(img_data[x,:,:].T)
+		im[2].set_data(img_data[:,:,y].T)
+		ct_im[0].set_data(ct_data[x,:,:].T)
+		ct_im[2].set_data(ct_data[:,:,y].T)
 		cursor[1][0].set_xdata([event.xdata, event.xdata])
 		cursor2[1][0].set_ydata([event.ydata, event.ydata])
 		cursor[2][0].set_xdata([event.xdata, event.xdata])
@@ -207,10 +217,12 @@ def on_click(event):
 
 	# If you clicked the third subplot
 	elif bb3.contains(fxy[0],fxy[1]):
-		im[0].set_data(img_data[event.xdata,:,:].T)
-		im[1].set_data(img_data[:,event.ydata,:].T)
-		ct_im[0].set_data(ct_data[event.xdata,:,:].T)
-		ct_im[1].set_data(ct_data[:,event.ydata,:].T)
+		current_slice[0] = event.xdata
+		current_slice[1] = event.ydata
+		im[0].set_data(img_data[x,:,:].T)
+		im[1].set_data(img_data[:,y,:].T)
+		ct_im[0].set_data(ct_data[x,:,:].T)
+		ct_im[1].set_data(ct_data[:,y,:].T)
 		cursor[2][0].set_xdata([event.xdata, event.xdata])
 		cursor2[2][0].set_ydata([event.ydata, event.ydata])
 		cursor[0][0].set_xdata([event.ydata, event.ydata])
@@ -219,10 +231,12 @@ def on_click(event):
 
 	# If you clicked the third subplot
 	elif bb4.contains(fxy[0],fxy[1]):
-		im[1].set_data(img_data[:,event.xdata,:].T)
-		im[2].set_data(img_data[:,:,event.ydata].T)
-		ct_im[1].set_data(ct_data[:,event.xdata,:].T)
-		ct_im[2].set_data(ct_data[:,:,event.ydata].T)
+		current_slice[1] = event.xdata
+		current_slice[2] = event.ydata
+		im[1].set_data(img_data[:,x,:].T)
+		im[2].set_data(img_data[:,:,y].T)
+		ct_im[1].set_data(ct_data[:,x,:].T)
+		ct_im[2].set_data(ct_data[:,:,y].T)
 
 		cursor[3][0].set_xdata([event.xdata, event.xdata])
 		cursor2[3][0].set_ydata([event.ydata, event.ydata])
@@ -231,9 +245,13 @@ def on_click(event):
 		cursor2[1][0].set_ydata([event.ydata, event.ydata])
 		cursor2[2][0].set_ydata([event.xdata, event.xdata])
 
+	print("Current slice: %3.2f %3.2f %3.2f"%(current_slice[0], current_slice[1], current_slice[2]))
 	plt.gcf().canvas.draw()
 
+cid2 = fig.canvas.mpl_connect('scroll_event',on_scroll)
 cid3 = fig.canvas.mpl_connect('button_press_event',on_click)
+cid = fig.canvas.mpl_connect('key_press_event', on_key)
 
 plt.show()
+fig.canvas.draw()
 #fig.canvas.setFocus()
