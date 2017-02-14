034de2a91f23b8661f552697ddc99344af3fd8df
Liberty Hamilton <libertyhamilton@gmail.com>
2017-02-13 16:11:53 -0800

    Trying to add axis labels

--- electrode_picker.py
+++ electrode_picker.py
@@ -5,32 +5,35 @@ import numpy as np
 import nibabel as nib
 import scipy.ndimage
 
-img = nib.load('/Applications/freesurfer/subjects/EC121_test/acpc/T1.nii')
-ct = nib.load('/Applications/freesurfer/subjects/EC121_test/CT/rCT.nii')
-affine = img.affine
-codes = nib.orientations.axcodes2ornt(nib.orientations.aff2axcodes(affine))
-img_data = nib.orientations.apply_orientation(img.get_data(), codes)
-voxel_sizes = nib.affines.voxel_sizes(affine)
-nx,ny,nz=np.array(img_data.shape, dtype='float')
+#img = nib.load('/Applications/freesurfer/subjects/EC121_test/acpc/T1.nii')
+#ct = nib.load('/Applications/freesurfer/subjects/EC121_test/CT/rCT.nii')
+# img = nib.load('/Applications/freesurfer/subjects/EC143/mri/orig.nii')
+# ct = nib.load('/Applications/freesurfer/subjects/EC143/CT/rCT.nii')
 
-inv_affine = np.linalg.inv(affine)
-img_clim = np.percentile(img_data, (1., 99.))
+# affine = img.affine
+# codes = nib.orientations.axcodes2ornt(nib.orientations.aff2axcodes(affine))
+# img_data = nib.orientations.apply_orientation(img.get_data(), codes)
+# voxel_sizes = nib.affines.voxel_sizes(affine)
+# nx,ny,nz=np.array(img_data.shape, dtype='float')
 
-#ct_data = ct.get_data()
-ct_codes =nib.orientations.axcodes2ornt(nib.orientations.aff2axcodes(ct.affine))
-ct_data = nib.orientations.apply_orientation(ct.get_data(), ct_codes)
+# inv_affine = np.linalg.inv(affine)
+# img_clim = np.percentile(img_data, (1., 99.))
 
-cx,cy,cz=np.array(ct.shape, dtype='float')
-current_slice = np.array([nx/2, ny/2, nz/2], dtype=np.int)
+# #ct_data = ct.get_data()
+# ct_codes =nib.orientations.axcodes2ornt(nib.orientations.aff2axcodes(ct.affine))
+# ct_data = nib.orientations.apply_orientation(ct.get_data(), ct_codes)
 
-# Resample both images to the highest resolution
-voxsz = (256, 256, 256)
-if ct.shape != voxsz:
-	print("Resizing voxels in CT")
-	ct_data = scipy.ndimage.zoom(ct_data, [voxsz[0]/cx, voxsz[1]/cy, voxsz[2]/cz])
-if img.shape != voxsz:
-	print("Resizing voxels in MRI")
-	img_data = scipy.ndimage.zoom(img_data, [voxsz[0]/nx, voxsz[1]/ny, voxsz[2]/nz])
+# cx,cy,cz=np.array(ct.shape, dtype='float')
+# current_slice = np.array([nx/2, ny/2, nz/2], dtype=np.int)
+
+# # Resample both images to the highest resolution
+# voxsz = (256, 256, 256)
+# if ct.shape != voxsz:
+# 	print("Resizing voxels in CT")
+# 	ct_data = scipy.ndimage.zoom(ct_data, [voxsz[0]/cx, voxsz[1]/cy, voxsz[2]/cz])
+# if img.shape != voxsz:
+# 	print("Resizing voxels in MRI")
+# 	img_data = scipy.ndimage.zoom(img_data, [voxsz[0]/nx, voxsz[1]/ny, voxsz[2]/nz])
 
 nx,ny,nz = [256, 256, 256]
 cx,cy,cz = [256, 256, 256]
@@ -50,8 +53,11 @@ cursor.append(plt.plot([ny/2, ny/2], [ax1.get_ylim()[0]+1, ax1.get_ylim()[1]-1],
 cursor2.append(plt.plot([ax1.get_xlim()[0]+1, ax1.get_xlim()[1]-1], [nz/2, nz/2], color=[0, 1, 0] ))
 plt.gca().invert_yaxis()
 ax1.axis([0,ny,0,nz])
-#ax1.set_xticks([])
-#ax1.set_yticks([])
+ax1.set_xticks([])
+ax1.set_yticks([])
+ax1.set_xlabel('Inferior')
+ax1.set_ylabel('Posterior')
+ax11 = ax1.twinx()
 
 ax2 = fig.add_subplot(222)
 ax2.set_axis_bgcolor('k')
@@ -61,8 +67,10 @@ cursor.append(plt.plot([nx/2, nx/2], [ax2.get_ylim()[0]+1, ax2.get_ylim()[1]-1],
 cursor2.append(plt.plot([ax2.get_xlim()[0]+1, ax2.get_xlim()[1]-1], [nz/2, nz/2], color=[0, 1, 0] ))
 plt.gca().invert_yaxis()
 ax2.axis([0,nx,0,nz])
-#ax2.set_xticks([])
-#ax2.set_yticks([])
+ax2.set_xticks([])
+ax2.set_yticks([])
+ax2.set_xlabel('Inferior')
+ax2.set_ylabel('Left')
 
 ax3 = fig.add_subplot(223)
 ax3.set_axis_bgcolor('k')
@@ -71,9 +79,11 @@ ct_im.append(plt.imshow(ct_data[:,:,nz/2].T, cmap=cm.hot, alpha=0.5, vmin=1000, 
 cursor.append(plt.plot([nx/2, nx/2], [ax3.get_ylim()[0]+1, ax3.get_ylim()[1]-1], color=[0, 1, 0] ))
 cursor2.append(plt.plot([ax3.get_xlim()[0]+1, ax3.get_xlim()[1]-1], [ny/2, ny/2], color=[0, 1, 0] ))
 plt.gca().invert_yaxis()
-#ax3.set_xticks([])
-#ax3.set_yticks([])
+ax3.set_xticks([])
+ax3.set_yticks([])
 ax3.axis([0,nx,0,ny])
+ax3.set_xlabel('Posterior')
+ax3.set_ylabel('Left')
 
 ax4 = fig.add_subplot(224)
 im.append(plt.imshow(ct_data.max(0).T, cmap=cm.gray, aspect='auto'))
@@ -88,11 +98,14 @@ ax4.axis([0,ny,0,nz])
 
 im.append(current_slice)
 
+plt.tight_layout()
+
 def on_key(event):
 	print('You pressed', event.key)
 	bb1=ax1.get_position()
 	bb2=ax2.get_position()
 	bb3=ax3.get_position()
+	bb4=ax4.get_position()
 	# Transform coordinates to figure coordinates
 	fxy = fig.transFigure.inverted().transform((event.x, event.y))
 	if current_slice[0]+event.step >= 0 and current_slice[0]+event.step <= nx:
