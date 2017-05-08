took the brain_color scalar adjustment out of the loop in ctmr_brain_plot.ctmr_gauss_plot, otherwise causes incorrect color scaling (as ben found)

img_pipe/plotting/ctmr_brain_plot.py
@@ -56,10 +56,10 @@ def ctmr_gauss_plot(tri, vert, color = (0.8, 0.8, 0.8), elecs = [], weights = []
             gauss_wt = np.nan_to_num(weights[i] * np.exp((-(b_x**2+b_z**2+b_y**2))/gsp)) #gaussian
             brain_color = brain_color + gauss_wt
 
-            #scale the colors so that it matches the weights that were passed in 
-            brain_color = brain_color * (np.abs(weights).max()/np.abs(brain_color).max())
-            if vmin==None and vmax==None:
-                vmin, vmax = -np.abs(brain_color).max(), np.abs(brain_color).max()
+        #scale the colors so that it matches the weights that were passed in 
+        brain_color = brain_color * (np.abs(weights).max()/np.abs(brain_color).max())
+        if vmin==None and vmax==None:
+            vmin, vmax = -np.abs(brain_color).max(), np.abs(brain_color).max()
 
     # plot cortex and begin display
     if new_fig: