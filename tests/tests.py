b155ba90a12170645b35e23f2bc7fc6d4fd06f10
Liberty Hamilton <libertyhamilton@gmail.com>
2017-08-19 08:25:21 -0500

    Still trying to fix relative import

--- tests/tests.py
+++ tests/tests.py
@@ -5,7 +5,7 @@ import img_pipe
 patient = img_pipe.freeCoG(subj = 'S1', hem = 'lh', fs_dir='', subj_dir='tests')
 
 # Test import of electrode picker
-from .SupplementalScripts import electrode_picker
+from ..SupplementalScripts import electrode_picker
 
 # Test import of plotting
-from .plotting import ctmr_brain_plot
+from ..plotting import ctmr_brain_plot
