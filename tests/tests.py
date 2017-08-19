1572ae31aca0e9a69f0ce0c4e8e3df14608b368b
Liberty Hamilton <libertyhamilton@gmail.com>
2017-08-19 08:11:57 -0500

    Added imports of plotting and elec picker fns

--- tests/tests.py
+++ tests/tests.py
@@ -3,3 +3,9 @@
 # Just test whether the import works
 import img_pipe
 patient = img_pipe.freeCoG(subj = 'S1', hem = 'lh', fs_dir='', subj_dir='tests')
+
+# Test import of electrode picker
+from img_pipe.SupplementalScripts import electrode_picker
+
+# Test import of plotting
+from img_pipe.plotting import ctmr_brain_plot
