daa730e8863249430a170fee70c885f151a8aa85
Liberty Hamilton <libertyhamilton@gmail.com>
2017-08-19 10:50:34 -0500

    Path changed

--- tests/tests.py
+++ tests/tests.py
@@ -9,10 +9,10 @@ import img_pipe
 patient = img_pipe.freeCoG(subj = 'S1', hem = 'lh', fs_dir='', subj_dir='tests')
 
 # Test import of electrode picker
-from .SupplementalScripts import electrode_picker
+from img_pipe.SupplementalScripts import electrode_picker
 
 # Test import of plotting
-from .plotting import ctmr_brain_plot
+from img_pipe.plotting import ctmr_brain_plot
 
 # Test whether mayavi works
 import mayavi
