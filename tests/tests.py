ed5357793dbb4d40464af0509911f0ced938f142
Liberty Hamilton <libertyhamilton@gmail.com>
2017-08-19 12:32:00 -0500

    Added path print

--- tests/tests.py
+++ tests/tests.py
@@ -2,6 +2,7 @@
 import sys
 from os import path
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+print(sys.path)
 
 # Just test whether the import works
 import img_pipe
@@ -9,10 +10,10 @@ import img_pipe
 patient = img_pipe.freeCoG(subj = 'S1', hem = 'lh', fs_dir='', subj_dir='.')
 
 # Test import of electrode picker
-from img_pipe.SupplementalScripts import electrode_picker
+from SupplementalScripts import electrode_picker
 
 # Test import of plotting
-from img_pipe.plotting import ctmr_brain_plot
+from plotting import ctmr_brain_plot
 
 # Test whether mayavi works
 import mayavi
