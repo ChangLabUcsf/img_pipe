b073718425f6a830b50e755ea59a50b497e2916d
Liberty Hamilton <libertyhamilton@gmail.com>
2017-08-18 22:17:10 -0500

    Changed fsdir and subjdir set

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 
+# Just test whether the import works
 import img_pipe
-import os
-os.environ['FREESURFER_HOME']=''
-os.environ['SUBJECTS_DIR']=''
-
-patient = img_pipe.freeCoG(subj = 'test', hem = 'lh')
+patient = img_pipe.freeCoG(subj = 'test', hem = 'lh', fs_dir='', subj_dir='')
