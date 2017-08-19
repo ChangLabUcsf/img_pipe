5ffc42c03ca4417fc8df7ad3be39cd301321afe5
Liberty Hamilton <libertyhamilton@gmail.com>
2017-08-19 14:40:27 -0500

    Added python versions

--- tests/tests.py
+++ tests/tests.py
@@ -3,9 +3,16 @@
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 
-# Just test whether the import works
-import img_pipe
-#from img_pipe import freeCoG
+print(sys.version_info)
+
+if sys.version_info < (3, 0):
+    print("Using Python 2.7")
+    # Just test whether the import works
+    import img_pipe
+else:
+    print("Using Python 3.5")
+    from img_pipe import img_pipe
+
 patient = img_pipe.freeCoG(subj = 'S1', hem = 'lh', fs_dir='', subj_dir='.')
 
 # Test whether mayavi works
@@ -15,5 +22,5 @@ from mayavi import mlab
 mlab.test_mesh()
 
 # Test import of plotting
-from ..plotting import ctmr_brain_plot
-
+import img_pipe.plotting as plotting
+import img_pipe.SupplementalScripts
