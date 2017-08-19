7df96b5a611bca2da9af9a0abf91cd6e346dbd08
Liberty Hamilton <libertyhamilton@gmail.com>
2017-08-18 22:30:10 -0500

    typo fix

--- tests/tests.py
+++ tests/tests.py
@@ -2,4 +2,4 @@
 
 # Just test whether the import works
 import img_pipe
-patient = img_pipe.freeCoG(subj = 'S1', hem = 'lh', fs_dir='', subj_dir='test')
+patient = img_pipe.freeCoG(subj = 'S1', hem = 'lh', fs_dir='', subj_dir='tests')
