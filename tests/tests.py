dfa2bdb21907d484156e960a2df0015cb32845d7
Liberty Hamilton <libertyhamilton@gmail.com>
2017-08-19 12:49:40 -0500

    Changed order of tests

--- tests/tests.py
+++ tests/tests.py
@@ -9,14 +9,15 @@ import img_pipe
 #from img_pipe import freeCoG
 patient = img_pipe.freeCoG(subj = 'S1', hem = 'lh', fs_dir='', subj_dir='.')
 
-# Test import of electrode picker
-from SupplementalScripts import electrode_picker
-
-# Test import of plotting
-from plotting import ctmr_brain_plot
-
 # Test whether mayavi works
 import mayavi
 from mayavi import mlab
 
 mlab.test_mesh()
+
+# Test import of electrode picker
+from ..SupplementalScripts import electrode_picker
+
+# Test import of plotting
+from ..plotting import ctmr_brain_plot
+
