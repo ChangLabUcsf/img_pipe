a81cbceaccdba9eef00c38eeb46a17302c2b5a64
root <luit_tauffer@hotmail.com>
2019-05-10 13:49:01 +0200

    return setup to previous version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(name = "img_pipe",
 	  packages = find_packages(),
 	  include_package_data = True,
 	  setup_requires=['cython','numpy','scipy'],
-	  install_requires=['numpy','scipy','pyvtk','mayavi','pymcubes','mne','nibabel','nipy','matplotlib<2.0.0','configparser','tqdm','nwbext_ecog'],
+	  install_requires=['numpy','scipy','pyvtk','mayavi','pymcubes','mne','nibabel','nipy','matplotlib<2.0.0','configparser','tqdm'],
 	  dependency_links=['http://www.vtk.org/files/release/6.3/VTK-6.3.0.tar.gz','https://downloads.sourceforge.net/project/pyqt/PyQt4/PyQt-4.12/PyQt4_gpl_mac-4.12.tar.gz?r=&ts=1487961590&use_mirror=superb-sea2'],
 	  cmdclass={'install':MyInstall},
 	  classifiers = [
